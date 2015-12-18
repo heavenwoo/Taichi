1359cfa1e1070964d19b6f9e925d532635bde2b1
Heaven Woo <heavenwoo@live.com>
2015-12-19 04:17:32 +0800

    init commit

--- src/Taichi/Core/Application.php
+++ src/Taichi/Core/Application.php
@@ -27,46 +27,6 @@ class Application
         $this->loadConfig();
 
         $this->initException();
-        $html = <<<TC
-<!DOCTYPE html>
-<html>
-
- <head>
-        <title>
-            some_resource
-        </title>
-    </head>
-    <body>
-  <div class="content">
-  <form method="HEAD" action="/delete" enctype="application/x-www-form-urlencoded">
-   <div>
-    <input id="title" name="title" placeholder="The Resource Title" type="text" value="The Resource Title" autofocus />
-   </div>
-   <div>
-    <textarea id="body" name="body" placeholder="The Resource Body" required>The Resource Body</textarea>
-   </div>
-   <div>
-    <input id="submit" name="submit" type="submit" value="Submit" />
-   </div>
-   <!-- Resource Settings -->
-   <ul>
-    <li>
-     <input id="listed" name="listed" type="checkbox" value="y" />
-    </li>
-    <li>
-     <input id="template" name="template" type="text" value="resource.html" />
-    </li>
-   </ul>
-  </form>
-  </div>
-    </body>
-</html>
-TC;
-//echo $html;
-        dd(file_get_contents('php://input', 'r'), $_SERVER, $_REQUEST,
-            $_REQUEST['HTTP_X_HTTP_METHOD_OVERRIDE'] ?? 'none');
-        router('get', '/', '@todo');
-        router()->get('/post', 'PostController#index');
     }
 
     public function registerAlias()
